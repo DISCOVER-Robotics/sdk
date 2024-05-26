f60ecd43272186d7c5d0e4621925ba5f0bab5c0e
Beiwen Tian <tianbeiwen88@gmail.com>
2024-05-26 16:00:27 +0800

    Release 2.8.3 1. upgrade version to 2.8.3 2. add keyboard reference for airbot_kbd_ctrl and airbot_sync 3. change angle_step when pressing 't' in airbot_kbd_ctrl 4. remove altering use_planning with 'y' in airbot_kbd_ctrl 5. add returning to zero positions with '`' in airbot_sync 6. pressing '/' now tries to reset error for both arms in airbot_sync 7. add replaying 100 times when pressing 'n' in airbot_sync 8. add example scripts for python sdk: test_joint_position.py, test_joint_speed.py, test_gravity_compensation.py, test_replay_mit.py 9. adjust default vel to 0.2 in python sdk

--- python/examples/test_gravity_compensation.py
+++ python/examples/test_gravity_compensation.py
@@ -3,13 +3,9 @@ import time
 import math
 
 urdf_path = airbot.AIRBOT_PLAY_WITH_GRIPPER_URDF
-bot = airbot.create_agent(urdf_path, "down", "can0", 1.0, "gripper", False)
+bot = airbot.create_agent(urdf_path, "down", "can0", 1.0, "gripper")
 
-bot.gravity_compensation()
+bot.manual_mode()
 time.sleep(5)
-bot.stop_gravity_compensation()
-for i in range(20):
-    bot.set_target_end(1)
-    time.sleep(0.05)
-    bot.set_target_end(0)
-    time.sleep(0.05)
+bot.online_mode()
+bot.set_target_joint_q([0, 0, 0, 0, 0, 0], blocking=True)
